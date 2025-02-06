ab194ab16aaef8f33e946de678438d714b92e31d
Sky_High <1901532022@qq.com>
2025-02-06 15:00:21 +0800

    完成c++测试题目

--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -1,5 +1,6 @@
 ﻿#include "../exercise.h"
-#include <cstring>
+#include<cstring>
+
 // READ: 类模板 <https://zh.cppreference.com/w/cpp/language/class_template>
 
 template<class T>
@@ -10,6 +11,10 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for(int i=0;i<4;i++){
+            shape[i]=shape_[i];
+            size *=shape[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -21,6 +26,13 @@ struct Tensor4D {
     Tensor4D(Tensor4D const &) = delete;
     Tensor4D(Tensor4D &&) noexcept = delete;
 
+    unsigned int get_index(unsigned int i0,unsigned int i1,unsigned int i2,unsigned int i3)const{
+        return i0*(shape[1]*shape[2]*shape[3])+
+            i1*(shape[2]*shape[3])+
+            i2*shape[3]+
+            i3;
+    }
+
     // 这个加法需要支持“单向广播”。
     // 具体来说，`others` 可以具有与 `this` 不同的形状，形状不同的维度长度必须为 1。
     // `others` 长度为 1 但 `this` 长度不为 1 的维度将发生广播计算。
@@ -28,6 +40,26 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        for(unsigned int i0=0;i0<shape[0];i0++){
+            for(unsigned int i1=0;i1<shape[1];i1++){
+                for(unsigned int i2=0;i2<shape[2];i2++){
+                    for(unsigned int i3=0;i3<shape[3];i3++){
+                        //计算种子
+                        unsigned int o0=others.shape[0]==1?0:i0;
+                        unsigned int o1=others.shape[1]==1?0:i1;
+                        unsigned int o2=others.shape[2]==1?0:i2;
+                        unsigned int o3=others.shape[3]==1?0:i3;
+
+                        //获取一维索引
+                        unsigned int this_idx=get_index(i0,i1,i2,i3);
+                        unsigned int other_idx=others.get_index(o0,o1,o2,o3);
+
+                        //广播加法
+                        data[this_idx]+=others.data[other_idx];
+                    }
+                }
+            }
+        }
         return *this;
     }
 };
